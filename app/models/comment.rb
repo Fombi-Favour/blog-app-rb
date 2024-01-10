225fb5547096f92d0075e3d493b0425eff3f6a8f
Fombi Favour <fombifavour@gmail.com>
2024-01-10 23:20:11 +0100

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_save :comment_counter_update
+
+  def comment_counter_update
+    post.increment!(:comment_counter)
+  end
 end
