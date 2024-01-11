db201b23ee9f2377ed276c5f5bfba584f44b03f0
Fombi Favour <fombifavour@gmail.com>
2024-01-11 17:51:08 +0100

    fix linter errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Comment, type: :model do
   describe Comment do
     let(:user1) { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
     let(:post1) { Post.new(author: user1, title: 'Hello', text: 'This is my first post') }
-    let(:comment1) { Comment.new(post: post1, user: user1, text: 'Hi Tom!')}
+    let(:comment1) { Comment.new(post: post1, user: user1, text: 'Hi Tom!') }
 
     it 'object should be valid' do
       expect(comment1).to be_valid
