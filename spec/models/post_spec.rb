af287fa2b1eefa2c83a3d09d8e57f940a179f033
Fombi Favour <fombifavour@gmail.com>
2024-01-11 17:44:32 +0100

    Create post_spec.rb

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe Post do
+    it 'title should not exceed 250 characters' do
+      post1 = Post.new(title: 'Hello' * 200)
+      post1.valid?
+      expect(post1.errors[:title]).to include('maximum should be 250 characters')
+    end
+
+    it 'title should be present' do
+      post1 = Post.new(title: 'first post')
+      post1.title = nil
+      expect(post1).not_to be_valid
+    end
+
+    it 'comments_counter should be an integer only' do
+      post1 = Post.new(comments_counter: 'x')
+      post1.valid?
+      expect(post1.errors[:comments_counter]).to include('is not an integer')
+    end
+
+    it 'likes_counter should be greater than or equal to zero' do
+      post1 = Post.new(likes_counter: -1)
+      post1.valid?
+      expect(post1.errors[:likes_counter]).to include('must be greater or equal to zero')
+    end
+  end
+end
