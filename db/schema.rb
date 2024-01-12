570f269d3244ac839a860cf7a675f4d799b54b9d
Fombi Favour <fombifavour@gmail.com>
2024-01-12 23:57:46 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_10_152834) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "text"
+    t.text "text"
     t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
@@ -35,7 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_10_152834) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "text"
+    t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
     t.bigint "author_id", null: false
