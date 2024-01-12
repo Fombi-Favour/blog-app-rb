0a156870f82d81c4aa0c9ebbbd3d23eedce2fe1b
Fombi Favour <fombifavour@gmail.com>
2024-01-12 23:57:57 +0100

    Update like_spec.rb

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 RSpec.describe Like, type: :model do
   describe Like do
     let(:user1) { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
-    let(:post1) { Post.new(author: user1, title: 'Hello', text: 'This is my first post') }
-    let(:like1) { Like.new(user: user1, post: post1) }
+    let(:post1) { Post.new(title: 'Hello', text: 'This is my first post', likes_counter: 1) }
+
+    before do
+      user1.save
+      post1.save
+    end
 
     it 'object should be valid' do
+      like1 = Like.new(author: user1, post: post1)
       expect(like1).to be_valid
     end
   end
