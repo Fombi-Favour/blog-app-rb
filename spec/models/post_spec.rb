3aae0d8683a7f13a0eea114c81a3f8ae489e6897
Fombi Favour <fombifavour@gmail.com>
2024-01-13 01:43:30 +0100

    Update post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe Post do
+    before(:all) do
+      user1 = User.new(name: 'name1', post_counter: 1)
+      user1.save
+
+      7.times do
+        Comment.create(author_id: user1.id, post_id: 1, text: 'text1')
+      end
+    end
+
     it 'title should not exceed 250 characters' do
       post1 = Post.new(title: 'Hello' * 200)
       post1.valid?
@@ -24,6 +33,18 @@ RSpec.describe Post, type: :model do
       post1 = Post.new(likes_counter: -1)
       post1.valid?
       expect(post1.errors[:likes_counter]).to include('must be greater than or equal to 0')
+    end
+
+    it 'post_counter should update' do
+      initial = User.find(1).post_counter
+      post1 = Post.new(author_id: 1, title: 'title1', text: 'text1')
+      post1.send(:post_counter_update)
+      expect(User.find(1).post_counter).to eq(initial + 1)
+    end
+
+    it 'should display recent comments' do
+      post1 = Post.new(author_id: 1, title: 'title1', text: 'text1')
+      expect(post1.recent_comments).to eq post1.comments.limit(5).order(created_at: :desc)
     end
   end
 end
