a2711268402e30165ec5ee2f6a9e30271e0d3a8e
Fombi Favour <fombifavour@gmail.com>
2024-01-13 01:43:36 +0100

    Update comment_spec.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Comment, type: :model do
   describe Comment do
     let(:user1) { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
     let(:post1) { Post.new(author: user1, title: 'Hello', text: 'This is my first post', comments_counter: 1) }
+    let(:comment1) { Comment.new(author: user1, post: post1) }
 
     before do
       user1.save
@@ -11,8 +12,11 @@ RSpec.describe Comment, type: :model do
     end
 
     it 'object should be valid' do
-      comment1 = Comment.new(author: user1, post: post1)
       expect(comment1).to be_valid
+    end
+
+    it 'comment_counter should update' do
+      expect(post1.comments_counter).to eq(1)
     end
   end
 end
