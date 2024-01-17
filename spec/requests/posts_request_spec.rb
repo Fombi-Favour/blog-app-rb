9bebe4754fa7e500515f400ad657658cf5b366b2
Fombi Favour <fombifavour@gmail.com>
2024-01-17 11:31:10 +0100

    Update posts_request_spec.rb

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -5,13 +5,15 @@ RSpec.describe 'Posts', type: :request do
     it 'renders a successful response' do
       get '/users/1/posts'
       expect(response).to be_successful
-    end
-
-    it 'response body should render a correct placeholder text' do
-      get '/users/1/posts'
-      expect(response).to have_http_status(:success)
       expect(response.status).to eq(200)
       expect(response.body).to include('All posts list')
+    end
+
+    it 'renders a successful response for a single post' do
+      get '/users/1/posts/1'
+      expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
+      expect(response.body).to include('Individual post')
     end
   end
 end
