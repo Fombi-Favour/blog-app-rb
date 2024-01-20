e4429f081ee4aadda0968417cc70547abcd32376
Fombi Favour <fombifavour@gmail.com>
2024-01-20 14:43:03 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,9 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :new, :create, :show]
-  end
-  resources :posts do
-    resources :comments, only: [:create]
-    resources :likes, only: [:create]
+    resources :posts, only: [:index, :new, :create, :show] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
 end
